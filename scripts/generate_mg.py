2fa802247d0a695abd676a5965f67e0102598a2d
Andrew Belt <andrewpbelt@gmail.com>
2020-03-03 06:46:13 -0500

    Add third-party modules to ModularGrid dataset. Add lots of offbrand panels for Mutable Instruments.

--- scripts/generate_mg.py
+++ scripts/generate_mg.py
@@ -6,33 +6,53 @@ import re
 import requests
 
 
+# Load existing dataset
 mg_filename = "ModularGrid-VCVLibrary.json"
-# with open(mg_filename) as f:
-# 	mg = json.load(f)
-mg = []
+with open(mg_filename) as f:
+	mg = json.load(f)
 
+# Iterate plugins
 for manifest_filename in glob.glob('manifests/*.json'):
-	slug = os.path.splitext(os.path.basename(manifest_filename))[0]
 	with open(manifest_filename) as f:
 		plugin = json.load(f)
+	plugin_slug = plugin['slug']
 
-	for module in plugin.get("modules", []):
-		mg_url = module.get("modularGridUrl")
+	# Iterate modules in plugin
+	for module in plugin.get('modules', []):
+		module_slug = module['slug']
+		mg_url = module.get('modularGridUrl')
 		if not mg_url:
 			continue
 
-		r = requests.get(mg_url)
-		m = re.search(r'data-module-id = "(\d+)"', r.text)
-		mg_id = m.group(1)
-		if not mg_id:
+		if [x for x in mg if x.get('mgUrl') == mg_url]:
 			continue
-		mg_id = int(mg_id)
 
-		library_url = f"https://library.vcvrack.com/{plugin['slug']}/{module['slug']}"
-		module_data = {"mgModuleId": mg_id, "vcvUrl": library_url}
-		mg.append(module_data)
-		print(mg_id)
-		print(library_url)
+		mg_data = {}
+		mg_data['mgUrl'] = mg_url
+		mg_data['pluginSlug'] = plugin_slug
+		mg_data['moduleSlug'] = module_slug
+		mg_data['vcvUrl'] = f"https://library.vcvrack.com/{plugin_slug}/{module_slug}"
+		mg.append(mg_data)
+		print(mg_data)
+
+
+# Iterate dataset
+for mg_data in mg:
+	if mg_data.get('mgModuleId'):
+		continue
+
+	# Scrape ModularGrid website for ID
+	mg_url = mg_data['mgUrl']
+	r = requests.get(mg_url)
+	m = re.search(r'data-module-id = "(\d+)"', r.text)
+	mg_id = m.group(1)
+	if not mg_id:
+		print(f"No ModularGrid ID found for {plugin_slug} {module_slug}")
+		continue
+	mg_id = int(mg_id)
+	mg_data['mgModuleId'] = mg_id
+	print(mg_data)
+
 
 with open(mg_filename, 'w') as f:
 	json.dump(mg, f, indent=2)
